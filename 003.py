c45088ab56f9c0290f819a0c9e002e3cb804667c
João Benedet <105388117+joaobenedetmachado@users.noreply.github.com>
2024-07-30 16:30:02 -0300

    first commit

---
+++ 003.py
@@ -0,0 +1,13 @@
+nome = input("seu nome >>")
+idade = int(input('sua idade >>'))
+
+if idade < 2:
+    print("Bercario")
+if idade >= 3 and idade <= 6:
+    print("Educacao Infantil")
+if idade >= 7 and idade <= 10:
+    print("Fund I")
+if idade >= 11 and idade <= 15:
+    print("Fund II")
+if idade >= 15 and idade <= 18:
+    print("Ensino Medio")
